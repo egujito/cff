0a007472fea6be2c2504634a426fb545e6100632
egujito <bessapachecolr@gamil.com>
2023-06-12 22:39:43 +0100

    Added base icons and user name length limit

--- cfetch.h
+++ cfetch.h
@@ -1,9 +1,12 @@
 #ifndef IMPL
 #define IMPL
 
+// standard limits of the following properties
 #define HOST_NAME_SIZE_LIM 253
-#define HOST_ICON 
-#define USER_ICON
-#define UPTIME_ICON 
+#define USER_NAME_SIZE_LIM 16
 
+// Make sure you use a nerd font in order for these to work
+#define HOST_ICON " "
+#define USER_ICON " "
+#define UPTIME_ICON " " 
 #endif
